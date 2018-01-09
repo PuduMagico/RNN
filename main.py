15b36b110fe91a704b40f141dea9f8b2d8d39b78
PuduMagico <nhigueraruiz@gmail.com>
2018-01-09 12:10:05 -0300

    Añadido plot de actividad
    Aun no se tienen los mismos resultados

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import net
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     rnn = net.net(0.01)
 
-    rnn.add_layer(4)
+    rnn.add_layer(10)
 #   rnn.add_layer(3)
 #    rnn.add_layer(1)
 
@@ -25,29 +27,35 @@ def main():
              neuron_counter += 1
         print "----------"
 
-    max_iters = 10000
+
+
+    max_iters = 5
     for i in range(max_iters):
         rnn.one_cycle()
         rnn.update_weights_net()
 
-    print " ************************************* "
+    layer1_initial_activity = rnn.layers[0].last_output
+    # layer2_activity = rnn.layers[1].last_output
 
-    print "Resumen de la red intermedia"
-    for i in range(len(rnn.layers)):
-        print 'Capa numero ' + str(i)
-        print "Output final de la capa (actividad)"
-        print rnn.layers[i].last_output
-
-        neuron_counter = 0
-        for neuron in rnn.layers[i].neurons:
-             print 'la neurona numero ' + str(neuron_counter) + " de esta capa tiene los siguientes parametros asociados:"
-             print "Pesos (weights)"
-             for weight in neuron.weights:
-                 print str(weight)
-             neuron_counter += 1
-        print "----------"
 
     print " ************************************* "
+
+    # print "Resumen de la red actual"
+    # for i in range(len(rnn.layers)):
+    #     print 'Capa numero ' + str(i)
+    #     print "Output final de la capa (actividad)"
+    #     print rnn.layers[i].last_output
+    #
+    #     neuron_counter = 0
+    #     for neuron in rnn.layers[i].neurons:
+    #          print 'la neurona numero ' + str(neuron_counter) + " de esta capa tiene los siguientes parametros asociados:"
+    #          print "Pesos (weights)"
+    #          for weight in neuron.weights:
+    #              print str(weight)
+    #          neuron_counter += 1
+    #     print "----------"
+    #
+    # print " ************************************* "
 
     max_iters = 10000
     for i in range(max_iters):
@@ -69,8 +77,21 @@ def main():
              neuron_counter += 1
         print "----------"
 
-    #Aqui viene lo de graficar resultados and so
+    print "************************"
+    print "plot section"
+    layer1_final_activity = rnn.layers[0].last_output
 
+    x = np.linspace(0,1,10)
+    plt.plot(x,layer1_initial_activity, label ="initial activity")
+    plt.plot(x,layer1_final_activity, label ="final activity")
+    plt.xlabel('Neuronas')
+    plt.ylabel('Actividad')
 
+    plt.title("test")
+
+    plt.legend()
+
+    plt.show()
+    print "the end"
 if __name__ == "__main__":
     main()
