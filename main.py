1133dcc40f099ff6fc7ab4175dde21eee1375711
PuduMagico <nhigueraruiz@gmail.com>
2018-01-09 12:38:05 -0300

    Añadido plot de pesos
    pcolormesh que muestra la relacion de pesos inter neuronas (1 capa)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 def main():
     rnn = net.net(0.01)
 
-    rnn.add_layer(10)
+    rnn.add_layer(20)
 #   rnn.add_layer(3)
 #    rnn.add_layer(1)
 
@@ -63,6 +63,8 @@ def main():
         rnn.update_weights_net()
 
     print "Resumen de la red final"
+
+    final_weights = []
     for i in range(len(rnn.layers)):
         print 'Capa numero ' + str(i)
         print "Output final de la capa (actividad)"
@@ -72,26 +74,34 @@ def main():
         for neuron in rnn.layers[i].neurons:
              print 'la neurona numero ' + str(neuron_counter) + " de esta capa tiene los siguientes parametros asociados:"
              print "Pesos (weights)"
+             final_weights.append(neuron.weights)
+
              for weight in neuron.weights:
                  print str(weight)
              neuron_counter += 1
         print "----------"
 
     print "************************"
+
     print "plot section"
     layer1_final_activity = rnn.layers[0].last_output
 
-    x = np.linspace(0,1,10)
+
+    plt.subplot(2, 1, 1)
+    x = np.linspace(0,1,20)
     plt.plot(x,layer1_initial_activity, label ="initial activity")
     plt.plot(x,layer1_final_activity, label ="final activity")
     plt.xlabel('Neuronas')
     plt.ylabel('Actividad')
 
     plt.title("test")
-
     plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.pcolormesh(final_weights)
 
     plt.show()
     print "the end"
+
 if __name__ == "__main__":
     main()
